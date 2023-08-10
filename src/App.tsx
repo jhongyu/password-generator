b017d5bb227df83625d4cf3eac46b43e9a6600f8
jihongyu <jhy172691619@gmail.com>
2023-08-10 10:06:24 +0800

    add result element

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { Copy } from 'react-feather';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [passwordGenerated, setPasswordGenerated] = useState(false);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main>
+      <p className="title">Password Generator</p>
+      <div className="result">
+        <div className="password-wrapper">
+          <p className={`password ${passwordGenerated ? 'active' : ''}`}>
+            12345678
+          </p>
+          <Copy className="copy" onClick={() => setPasswordGenerated(true)} />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </main>
+  );
 }
 
-export default App
+export default App;
